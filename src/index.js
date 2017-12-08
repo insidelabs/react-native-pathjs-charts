0eb50baebd4dcad405ebe275422333706742c1d2
Daniel Medina <dmkmedina@gmail.com>
2017-12-08 11:59:44 +0100

    force paths-js to 0.3.4, remove the polyfills from the index

--- src/index.js
+++ src/index.js
@@ -16,23 +16,15 @@ SPDX-Copyright: Copyright (c) Capital One Services, LLC
 SPDX-License-Identifier: Apache-2.0
 */
 
-import Pie from './Pie'
-import Tree from './Tree'
-import Radar from './Radar'
-import Bar from './Bar'
-import SmoothLine from './SmoothLine'
-import StockLine from './StockLine'
-import Scatterplot from './Scatterplot.js'
+import Pie from './Pie';
+import Tree from './Tree';
+import Radar from './Radar';
+import Bar from './Bar';
+import SmoothLine from './SmoothLine';
+import StockLine from './StockLine';
+import Scatterplot from './Scatterplot.js';
 
-import 'core-js/es6/symbol'
-import 'core-js/fn/symbol/iterator'
+//import 'core-js/es6/symbol'
+//import 'core-js/fn/symbol/iterator'
 
-export {
-  Pie,
-  Tree,
-  Bar,
-  SmoothLine,
-  StockLine,
-  Scatterplot,
-  Radar
-}
+export { Pie, Tree, Bar, SmoothLine, StockLine, Scatterplot, Radar };
